1d46d8f5f1a38adf497fb30f085bdd27ac364714
Jair Escobar <evitalires@gmail.com>
2019-06-06 00:30:15 -0500

    Updates

---
+++ precache-manifest.507354405da817287617454f7903c370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac94811fcdcc329609602c67cc4f543",
+    "url": "/platzi-video/index.html"
+  },
+  {
+    "revision": "f584023244ad85e3761c",
+    "url": "/platzi-video/static/css/main.c3bded72.chunk.css"
+  },
+  {
+    "revision": "c93048c3a6b0d601ecf4",
+    "url": "/platzi-video/static/js/2.18a0138a.chunk.js"
+  },
+  {
+    "revision": "f584023244ad85e3761c",
+    "url": "/platzi-video/static/js/main.22baa34e.chunk.js"
+  },
+  {
+    "revision": "4987990c599a4f9c5016",
+    "url": "/platzi-video/static/js/runtime~main.c0dfe903.js"
+  }
+]);
